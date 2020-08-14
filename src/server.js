1c19f4db7fb91213139eea4d2e31846c8b84617c
basedantoni <anthony.mercado300@gmail.com>
2020-08-14 13:50:56 -0500

    pg connection succesful

--- src/server.js
+++ src/server.js
@@ -3,16 +3,28 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import { Client } from 'pg';
 
 import users from './routes/api/users';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const client = new Client();
+
+async function pgConnect() {
+  await client.connect();
+
+  const res = await client.query('SELECT * FROM users')
+  await client.end();
+  console.log(res)
+}
+
+pgConnect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/users', users);
 
