06089a36bb3eb00059cec81436756ed8500f0105
basedantoni <anthony.mercado300@gmail.com>
2020-08-15 16:40:04 -0500

    implemented hashing

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import pool from '../../db';
-import bcrypt from 'bcrypt';
+import bcrypt, { hash } from 'bcrypt';
 
 const router = Router();
 
@@ -20,10 +20,17 @@ router.get('/:userId', (req, res) => {
 router.post('/register', async(req, res) => {
   try {
     const { name, password, email } = req.body;
+    let passwordHashed;
+
+    bcrypt.hash(password, 4, (err, hash) => {
+      err ? console.error(err) : passwordHashed = hash
+    })
+
     const text = 'INSERT INTO users(password, name, email) VALUES ($1, $2, $3)';
-    const values = [password, name, email];
-    // const result = await pool.query(text, values)
-    // console.log(result)
+    const values = [passwordHashed, name, email];
+    const result = await pool.query(text, values)
+    console.log(result)
+    res.json(result);
   } catch (err) {
     console.error(err.message);
   }
