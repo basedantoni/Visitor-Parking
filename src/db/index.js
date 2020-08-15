f22e03433de06a9103398bff3ae768c581f2c049
basedantoni <anthony.mercado300@gmail.com>
2020-08-15 15:12:36 -0500

    successfully connected to db

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE
+});
+
+export default pool;
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,41 +1,32 @@
 import { Router } from 'express';
+import pool from '../../db';
+import bcrypt from 'bcrypt';
 
 const router = Router();
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
- 
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
-
-router.get('/', (req, res) => {
-  return res.send(Object.values(users))
+router.get('/', async(req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users')
+    res.send(result)
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 router.get('/:userId', (req, res) => {
   return res.send(users[req.params.userId])
 });
 
-router.post('/', (req, res) => {
-  return res.send('POST HTTP method on user resource')
+router.post('/register', async(req, res) => {
+  try {
+    const { name, password, email } = req.body;
+    const text = 'INSERT INTO users(password, name, email) VALUES ($1, $2, $3)';
+    const values = [password, name, email];
+    // const result = await pool.query(text, values)
+    // console.log(result)
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 router.put('/:userId', (req, res) => res.send('PUT HTTP method on user resource'));
