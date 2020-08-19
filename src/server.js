dfb9acffa7583c43f33693be8e3e0e50847d9751
basedantoni <anthony.mercado300@gmail.com>
2020-08-19 10:36:50 -0500

    set up template engine

--- src/server.js
+++ src/server.js
@@ -9,12 +9,23 @@ import passport from 'passport';
 import users from './routes/api/users';
 import visitors from './routes/api/visitors';
 import cars from './routes/api/cars';
+import { __express } from 'hbs';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 require('./config/passport');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('html', __express);
+
+app.get('/', (req, res) => res.render('index.html', {name: 'anthony'}));
+app.get('/profile', (req, res) => res.render('profile.html'));
+app.get('/newvisitor', (req, res) => res.render('newvisitor.html'));
+app.get('/register', (req, res) => res.render('register.html'));
+
+//app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(cors());
