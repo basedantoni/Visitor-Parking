1b093fcdeb6f1081ee6fc0eb79cd23daffb49e52
basedantoni <anthony.mercado300@gmail.com>
2020-08-20 14:34:28 -0500

    :sparkles: Basic CRUD
    Finished the basic crud operations

--- public/js/newCar.js
+++ public/js/newCar.js
@@ -1,8 +1,18 @@
 const cardButton = document.querySelectorAll('.car-btn');
+const bookButton = document.querySelectorAll('.book-btn');
 
 cardButton.forEach(btn => {
   btn.addEventListener('click', async e => {
     res = await fetch(`/api/visitors/vuid?name=${e.srcElement.id}`)
     location.href = "/newcar";
   });
+})
+
+bookButton.forEach(btn => {
+  btn.addEventListener('click', e => {
+    const isBooking = confirm(`Book ${e.srcElement.id}?`);
+    if(isBooking) {
+      console.log('YAY')
+    }
+  })
 })
